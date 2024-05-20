803c8dc12fd326ac97bed40d5e3011b0f77d5e97
GAYATRI DAHAKE <142529640+Gayatri2211-23@users.noreply.github.com>
2024-05-20 15:43:09 +0530

    Add files via upload

---
+++ light.js
@@ -0,0 +1,11 @@
+  function light(){
+    let btn=document.querySelector('.btn');
+    
+    let body=document.querySelector('body');
+     body.classList.toggle("on");
+     audio.play();
+ 
+  }
+
+   
+  
